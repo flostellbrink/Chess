902e9eee13def69d7a3ee15ec79b0d96b4727d67
Florian Stellbrink <florian@stellbr.ink>
2019-02-12 17:31:44 +0100

    Expose config as emscripten binding

--- src/Config.cpp
+++ src/Config.cpp
@@ -1,3 +1,4 @@
+#include <emscripten/bind.h>
 #include "Config.h"
 
 /*
@@ -21,3 +22,16 @@ bool Config::change_theme = false;
 bool Config::new_game = false;
 bool Config::undo_turn = false;
 bool Config::demo = false;
+
+EMSCRIPTEN_BINDINGS(static_member)
+{
+  emscripten::class_<Config>("Config")
+    .class_property("geo_resolution_horizontal", &Config::geo_resolution_horizontal)
+    .class_property("geo_resolution_vertical", &Config::geo_resolution_vertical)
+    .class_property("geo_changed", &Config::geo_changed)
+    .class_property("ai", &Config::ai)
+    .class_property("change_theme", &Config::change_theme)
+    .class_property("new_game", &Config::new_game)
+    .class_property("undo_turn", &Config::undo_turn)
+    .class_property("demo", &Config::demo);
+}
